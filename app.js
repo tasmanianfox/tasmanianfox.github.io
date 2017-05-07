7a998ac332c19fe0e933550be13b8a2b9ebcc479
Sergei Belyakov <tasmanianfox@gmail.com>
2017-05-07 20:37:19 +0300

    Added a password generator

--- app.js
+++ app.js
@@ -21,19 +21,67 @@ app.config(function($routeProvider) {
 });
 
 app.controller('MainController', function($scope) {
-	
+
 });
 
 app.controller('JavascriptsHashController', function($scope) {
-	
+
 });
 
 app.controller('JavascriptsRsaController', function($scope) {
-	
+
 });
 
 app.controller('JavascriptsPasswordController', function($scope) {
-	
+	$scope.settings = {
+		length: 10,
+		amount: 10,
+    containsLowerCaseLetters: true,
+    containsUpperCaseLetters: true,
+    containsNumbers: true,
+    containsSpecialCharacters: true,
+	}
+  $scope.passwords = ["(Please press a \"Generate now!\" button)"];
+
+  var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '0123456789';
+  var specialCharacters = '!@#$%^&*(),.<>{}[]~';
+
+  // Copy-pasted from: https://learn.javascript.ru/task/random-int-min-max
+  function randomInteger(min, max) {
+      var rand = min - 0.5 + Math.random() * (max - min + 1)
+      rand = Math.round(rand);
+      return rand;
+  }
+
+	$scope.generate = function($event) {
+		$event.preventDefault();
+    var dictionary = '';
+    if(true == $scope.settings.containsLowerCaseLetters) {
+        dictionary += lowerCaseLetters;
+    }
+    if(true == $scope.settings.containsUpperCaseLetters) {
+        dictionary += upperCaseLetters;
+    }
+    if(true == $scope.settings.containsNumbers) {
+        dictionary += numbers;
+    }
+    if(true == $scope.settings.containsSpecialCharacters) {
+        dictionary += specialCharacters;
+    }
+    var dictionaryLength = dictionary.length;
+    $scope.passwords = [];
+		for(var passwordIndex = 0; passwordIndex < $scope.settings.amount; passwordIndex++) {
+      var password = '';
+      while(password.length < $scope.settings.length) {
+        var index = randomInteger(0, dictionaryLength);
+        var character = dictionary.substring(index, index + 1);
+        password += character;
+      }
+      $scope.passwords[$scope.passwords.length] = password;
+    }
+	}
 });
 
 $(document).on('click', '[data-noclick]', function(e) {
